41ee3a094e7bba39cb839e08fe62ed886c18aa3b
Scott Draper <admin@scottdraper.io>
2026-03-04 16:01:00 -0700

    Bump version to 2.0.4

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ except ImportError:
     try:
         from src.transmog import __version__ as release
     except ImportError:
-        release = "2.0.3"
+        release = "2.0.4"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
